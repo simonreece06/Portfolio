bc04538688172480a5a6e6801ea311ba3886df76
Simon Reece <simonreece06@hotmail.com>
2026-02-21 03:26:33 +0000

    Added the project buttons, my god

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//DARK THEME
+
 const toggleButton = document.getElementById("darkToggle");
 
 // Load saved theme
@@ -17,4 +19,36 @@ toggleButton.addEventListener("click", () => {
         localStorage.setItem("theme", "light");
         toggleButton.textContent = "Dark Mode";
     }
+});
+
+//CLICK PROJECTS TO CHANGE THE BACKGROUND COLOUR?
+
+
+const projectButton = document.querySelectorAll(".project-card");
+
+
+projectButton.forEach(card => {
+    const button = card.id
+    //checking local storage
+    if (localStorage.getItem(button) === "selected") {
+       card.classList.add("selected");
+    }
+
+    card.addEventListener("click", () => {
+        //first we loop and reset all to zero so to speak
+        projectButton.forEach(otherCard => {
+            if (otherCard.id != button) {
+                otherCard.classList.remove("selected");
+                localStorage.removeItem(otherCard.id); //ignoring our card in question ()
+            }         
+        })
+        card.classList.toggle("selected");
+        if (localStorage.getItem(card.id) === "selected") {
+            localStorage.removeItem(card.id); //this loop is ensuring localstorage is correctg
+        } else {
+            localStorage.setItem(card.id, "selected"); //setting local storage to the correct thing
+        }        
+
+    });
+
 });
