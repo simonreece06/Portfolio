9bf0bf6a1611175c0a174fc9b56dccb58571f720
Simon Reece <simonreece06@hotmail.com>
2026-02-23 03:09:38 +0000

    added the counter buttons

--- js/script.js
+++ js/script.js
@@ -52,3 +52,51 @@ projectButton.forEach(card => {
     });
 
 });
+
+
+// a button that will change colour every time you click and also show how many times it has been clicked, saved in localStorage
+
+const clickyButton = document.querySelector("#counter");
+const resetButton = document.querySelector("#resetButton");
+
+ //now for random colour. random colour function copy pasted from chatgpt
+
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+//make the colour load of the button
+if (localStorage.getItem("clickyColour")) {
+    clickyButton.style.backgroundColor = localStorage.getItem("clickyColour");
+}
+
+//make reset button appear if count is not 0;
+if (localStorage.getItem("count") != null) {
+    resetButton.style.display="inline-block";
+}
+
+clickyButton.addEventListener("click", () => {
+    let counter = localStorage.getItem("count");
+    counter = Number(counter) + 1;
+    clickyButton.textContent = "Click Me! " + counter;
+    localStorage.setItem("count", counter);
+    resetButton.style.display = "inline-block";
+    const newColour = getRandomColor();
+    clickyButton.style.backgroundColor = newColour;
+
+    localStorage.setItem("clickyColour", newColour);
+})
+
+//reset button functionality
+resetButton.addEventListener("click", () => {
+    localStorage.setItem("count", 0);
+    clickyButton.textContent = "Click Me!";
+    resetButton.style.display = "none";
+    clickyButton.style.backgroundColor = "red";
+
+ })
+
